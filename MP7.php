240b654a4654a7fe47da6906ae00690a35cecb4e
Tricia Lorrainne Rullan <tlgrullan@klab.com>
2014-01-02 18:54:26 +0800

    PHP Mini Program 7: This finds the minimum integer stored in an array.

---
+++ MP7.php
@@ -0,0 +1,33 @@
+<?php
+//Gets the minimum integer stored in an array
+
+    echo "Enter 10 integers. Among of these, minimum integer will be identified.\n";
+    echo "Integer 1: ";
+    $readInt1 = fgets(STDIN);
+    echo "Integer 2: ";
+    $readInt2 = fgets(STDIN);
+    echo "Integer 3: ";
+    $readInt3 = fgets(STDIN);
+    echo "Integer 4: ";
+    $readInt4 = fgets(STDIN);
+    echo "Integer 5: ";
+    $readInt5 = fgets(STDIN);
+    echo "Integer 6: ";
+    $readInt6 = fgets(STDIN);
+    echo "Integer 7: ";
+    $readInt7 = fgets(STDIN);
+    echo "Integer 8: ";
+    $readInt8 = fgets(STDIN);
+    echo "Integer 9: ";
+    $readInt9 = fgets(STDIN);
+    echo "Integer 10: ";
+    $readInt10 = fgets(STDIN);
+
+    $arrayInt = array((int)$readInt1, (int)$readInt2, (int)$readInt3, (int)$readInt4, (int)$readInt5, (int)$readInt6, (int)$readInt7, (int)$readInt8, (int)$readInt9, (int)$readInt10);
+    sort($arrayInt);
+    
+    echo "Minimum integer: ";
+    echo $arrayInt[0];
+    echo "\n";
+
+?>
